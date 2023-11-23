809d1f1a826edf5ee8fb654c4f79abbb9d606b3b
n005 <n005char@gmail.com>
2023-11-23 23:14:23 +0100

    bot VS bot test

--- karmaka/src/fr/utt/karmaka/Aleatoire.java
+++ karmaka/src/fr/utt/karmaka/Aleatoire.java
@@ -2,7 +2,8 @@ package fr.utt.karmaka;
 
 public class Aleatoire implements Strategie {
 	public void jouer(Joueur joueur) {
-		int numCarte = 1; 
+		joueur.piocher();
+		int numCarte = 0; 
 		joueur.jouerPoints(numCarte);
 	}
 
--- karmaka/src/fr/utt/karmaka/Main.java
+++ karmaka/src/fr/utt/karmaka/Main.java
@@ -3,17 +3,25 @@ package fr.utt.karmaka;
 public class Main {
 	public static void main(String[] args) {
 		Partie partie = new Partie();
-		Joueur joueur1 = partie.creerJoueur();
+		//Joueur joueur1 = partie.creerJoueur();
 		JoueurVirtuel joueurVirtuel1 = new JoueurVirtuel("joueurvirtuel");
+		JoueurVirtuel joueurVirtuel2 = new JoueurVirtuel("joueurvirtuel2");
 		
 		partie.ajouterUnJoueur(joueurVirtuel1);
-		partie.ajouterUnJoueur(joueur1);
+		//partie.ajouterUnJoueur(joueur1);
+		partie.ajouterUnJoueur(joueurVirtuel2);
 		
-		partie.DonnerCarteMiseEnPlace(joueur1, joueurVirtuel1);
+		partie.DonnerCarteMiseEnPlace(joueurVirtuel1, joueurVirtuel2);
 		
-		while (!(joueur1.aGagner() || joueurVirtuel1.aGagner())) {
-			partie.jouerUnTour(joueur1);
+		while (!(joueurVirtuel2.aGagner() || joueurVirtuel1.aGagner())) {
+			//partie.jouerUnTour(joueur1);
+			System.out.println("C'est au tour de "+joueurVirtuel1.getNom());
+			System.out.println(joueurVirtuel1);
 			joueurVirtuel1.jouerBot();
+			
+			System.out.println("C'est au tour de "+joueurVirtuel2.getNom());
+			System.out.println(joueurVirtuel2);
+			joueurVirtuel2.jouerBot();
 		}
 		
 		
